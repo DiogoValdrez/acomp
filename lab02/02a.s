a0d15a89fe35e6e59f8c8b10296187aa5b028328
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-04-24 18:45:51 +0100

    Added prob04

--- lab02/02a.s
+++ lab02/02a.s
@@ -1,52 +1,55 @@
 	.data
 
+a: 	.word 1
+b: 	.word 1
+c:  .word 1
+
 	.text
-	li a2, 0
-	li a3, 0
-	addi sp, sp, -12
-	sw a2, 4(sp)
-	sw a3, 0(sp)
-	jal rede_neuronal_xor
-	lw a3, 0(sp)
-	lw a2, 4(sp)	
-	addi sp, sp, 12
+main:
+	lw a2, a
+	lw a3, b
+	lw a4, c
 
-	li a2, 0
-	li a3, 1
-	addi sp, sp, -12
-	sw a2, 4(sp)
-	sw a3, 0(sp)
+	addi sp, sp, -16
+	sw a2, 8(sp) #a
+	sw a3, 4(sp) #b
+	sw a4, 0(sp) #c
 	jal rede_neuronal_xor
-	lw a3, 0(sp)
-	lw a2, 4(sp)	
-	addi sp, sp, 12
+	lw a5, 0(sp)
+	lw a4, 4(sp)
+	lw a3, 8(sp)
+	lw a2, 12(sp)
+	addi sp, sp, 20
 
-	li a2, 1
-	li a3, 0
-	addi sp, sp, -12
-	sw a2, 4(sp)
-	sw a3, 0(sp)
+
+	addi sp, sp, -20
+	sw a3, 0(sp) #xor(a,b)
+	sw a4, 4(sp) #c
+	sw a5, 8(sp) #b
 	jal rede_neuronal_xor
-	lw a3, 0(sp)
-	lw a2, 4(sp)	
-	addi sp, sp, 12
+	lw a6, 0(sp) #xor(xor(a,b),c)
+	sw a3, 4(sp) #xor(a,b)
+	lw a4, 8(sp) #b
+	addi sp, sp, 24
 
-	li a2, 1
-	li a3, 1
-	addi sp, sp, -12
-	sw a2, 4(sp)
-	sw a3, 0(sp)
-	jal rede_neuronal_xor
-	lw a3, 0(sp)
-	lw a2, 4(sp)	
-	addi sp, sp, 12
+	mv a2, a6 
+	mv a3, a6
+	li a4, -1	
+	li a5, -1
+	li a6, 1
+	jal neuronio	
 
+	li x17,1
+	ecall
 	li x17,10
 	ecall
+
 rede_neuronal_xor:
-	sw ra, 8(sp)
-	lw a2, 4(sp) #x1
-	lw a3, 0(sp) #x2
+
+	sw ra, 12(sp) #salvaguarda
+
+	lw a2, 8(sp) #x1
+	lw a3, 4(sp) #x2
 	li a4, 2 #w1
 	li a5, -2 #w2
 	li a6, -1	
@@ -61,12 +64,12 @@ rede_neuronal_xor:
 	mv a3, a0 #x2
 	li a4, 2 #w1
 	jal neuronio
-	
-	li x17,1
-	ecall
-	
-	lw ra, 8(sp)
-	ret
+		
+	addi sp, sp, -4
+	sw a0, 0(sp)
+	lw ra, 16(sp)
+
+	jalr x0,x1,0
 neuronio:
 	addi sp, sp, -28 #salvaguarda
 	sw ra, 24(sp)
@@ -137,17 +140,3 @@ end:
 	addi sp, sp, -4 #add do resultado á pilha
 	sw t2, 0(sp)
 	jalr x0,x1,0
-	sw s2, 8(sp)
-	lw a6, 12(sp)
-	lw a5, 16(sp)
-	lw a4, 20(sp)
-	lw a3, 24(sp)
-	lw a2, 28(sp)
-	lw ra, 32(sp)
-	addi sp, sp, 36
-	
-	add t0, t6, a7
-	add t0, t0, a6
-	blt t0, zero, else #if/else presente no codigo em c
-	li a0, 1
-	jalr x0,x1,0 
---
+++ prob04/02_a_b.s
@@ -0,0 +1,75 @@
+.data
+
+on: .word 0
+
+.text
+
+
+int:
+	addi sp, sp, -8
+	sw x1, 4(sp)
+	sw x10, 0(sp)
+	
+	csrrw x10, scause, zero
+
+	addi x10, x10, -3
+	bne x10, zero, next
+	jal checkTemp
+	
+
+
+next:
+	# ...
+	lw x1, 4(sp)
+	lw x10, 0(sp)
+	addi sp, sp, 8
+	sret
+
+checkTemp:
+	addi sp, sp, -8
+	sw x10, 4(sp)
+	sw x11, 0(sp)
+	
+
+
+	lw x10, on
+	beq x10, x0, off
+
+	li x11, 40
+	li x10, 0xffff000c
+	lw x10, 0(x10)
+
+	bgt x10, x11, end
+	li x10, 0xffff000a
+	sw zero, 0(x10) # desliga ventoinha
+	j end
+
+off:
+	li x11, 50
+	li x10, 0xffff000c
+	lw x10, 0(x10)
+	
+	blt x10, x11, end
+	li x11, 1
+	li x10, 0xffff000a
+	sw x11, 0(x10) # ligar ventoinha
+
+end:
+	lw x10, 4(sp)
+	lw x11, 0(sp)
+	addi sp, sp, 8
+	ret
+	
+	
+#5c
+
+#scause <- 3
+#sepc <- 0x58 ou 0x64
+#pc <- stvect
+#SPIE <- 0
+
+#depois da interrupção terminar
+#pc <- sepc
+#SPIE <-1
+
+
